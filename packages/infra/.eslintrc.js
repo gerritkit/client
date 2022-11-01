60a970bbbcc5af8af5756712dfcdd3344454c931
r.leginkov <rodion.leg.work@gmail.com>
2022-11-01 19:35:13 +0300

    Move to gh actions

---
+++ packages/infra/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    extends: [
+      'eslint-config-qiwi',
+      'prettier'
+    ],
+    overrides: [{
+      files: ['./src/**/*.ts'],
+      rules: {
+        'unicorn/consistent-function-scoping': 'off',
+        'unicorn/no-null': 'off',
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'camelcase': 'off',
+        'simple-import-sort/imports': 'off',
+        'no-template-curly-in-string': 'off',
+        'unicorn/escape-case': 'off'
+      },
+    }]
+  };
+  
